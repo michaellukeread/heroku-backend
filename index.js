a6c5f7d341d7edae8130bacf6196630047a4d36e
michaellukeread <mread100@gmail.com>
2022-03-14 20:00:26 +1100

    CORS fix

--- index.js
+++ index.js
@@ -7,6 +7,28 @@ let example = "starting"
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000")
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  )
+
+  // Request headers you wish to allow
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type")
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true)
+
+  // Pass to next layer of middleware
+  next()
+})
+
 app.get("/", (req, res) => res.send("Hello World!"))
 app.get("/api/v1/test", (req, res) => res.send({ string: example }))
 app.post("/api/v1/:string", (req, res) => (example = req.params.string))
