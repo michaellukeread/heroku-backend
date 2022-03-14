bff21d805c48a25a3680505f0b71e58e221d6105
michaellukeread <mread100@gmail.com>
2022-03-14 17:33:00 +1100

    basic get and post

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+let example = "starting"
+
 app.get("/", (req, res) => res.send("Hello World!"))
+app.get("/api/v1/test", (req, res) => res.send({ string: example }))
+app.post("/api/v1/:string", (req, res) => (example = req.params.string))
 
 app.listen(process.env.PORT || port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
